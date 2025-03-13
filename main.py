7ac93d80a131963d835be703722130ac43979d69
DaGeek <Da-Geek@users.noreply.github.com>
2025-03-13 19:39:35 +0000

    Fix for /config.ini path
    Correcting the PATH of the `config.ini` file so it doesn't need to be in `/config.ini`

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import configparser
 
 # Load configuration from ini file
 config = configparser.ConfigParser()
-config.read('/config.ini')
+config.read('config.ini')
 
 # Get folder paths from config or use default
 WATCH_DIR = Path(config.get('folders', 'watch_dir', fallback="/WATCHED"))
